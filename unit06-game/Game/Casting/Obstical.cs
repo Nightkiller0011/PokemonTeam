056b9ae41494681f5952b43ed39b50cda4499686
Tyler English <eng20007@byui.edu>
2022-03-22 09:52:09 -0600

    Obsticals and enemies

--- unit06-game/Game/Casting/Obstical.cs
+++ unit06-game/Game/Casting/Obstical.cs
@@ -4,16 +4,27 @@ using System;
 namespace Unit06.Game.Casting
 {
     /// <summary>
-    /// <para>An Obstical in the game to inhibit the Player.</para>
+    /// <para>The Player in the game.</para>
     /// </summary>
     public class Obstical : Actor
     {
-
-        /// <summary>
-        /// Constructs a new instance of an Obstical.
-        /// </summary>
-        public Obstical()
+        private string _message = "O";
+        public Obstical(string obsticle)
         {
+            // create the player
+            Cast cast = new Cast();
+
+            SetText(_message);
+
+            SetFontSize(Constants.FONT_SIZE);
+
+            if (obsticle == "obsticle")
+            {
+                SetColor(Constants.RED);
+                SetPosition(new Point(300, 575));
+            }
+            
         }
+
     }
 }
