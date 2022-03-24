a149f2ef0048e2612458fe056d0b4fd4bcb8d584
Tyler English <eng20007@byui.edu>
2022-03-24 09:23:33 -0600

    enemies in 15 size

--- unit06-game/Game/Casting/enemy.cs
+++ unit06-game/Game/Casting/enemy.cs
@@ -11,7 +11,14 @@ namespace Unit06.Game.Casting
         //private string _message1 = "E";
         public Enemy(string enemy, string messages)
         {
+            int x = 0;
+            int y = 0;
+
             Random rand = new Random();
+
+            x = rand.Next(0,60) * 15;
+            y = rand.Next(3,40) * 15;
+
             // create the player
             Cast cast = new Cast();
 
@@ -22,17 +29,17 @@ namespace Unit06.Game.Casting
             if (enemy == "enemy")
             {
                 SetColor(Constants.RED);
-                SetPosition(new Point(rand.Next(0,900), rand.Next(0,600)));
+                SetPosition(new Point(x, y));
             }
             else if(enemy == "enemy2")
             {
                 SetColor(Constants.RED);
-                SetPosition(new Point(rand.Next(0,900), rand.Next(0,600)));
+                SetPosition(new Point(x,y));
             }
             else if (enemy == "enemy3")
             {
                 SetColor(Constants.RED);
-                SetPosition(new Point(rand.Next(0,900), rand.Next(0,600)));
+                SetPosition(new Point(x,y));
             }
             {
                 
